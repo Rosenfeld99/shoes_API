620466261bf859f50bf89eae95d2dacb3eff7985
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2023-05-23 20:27:56 +0300

    Add files via upload

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 // console.log(process.env.COLECTION_DB);
 exports.config = {
-    URL_DB : process.env.URL_DB,
-    COLECTION_DB : process.env.COLECTION_DB,
-    TOKEN_SECRET : process.env.TOKEN_SECRET
-}
+  URL_DB: process.env.URL_DB,
+  COLECTION_DB: process.env.COLECTION_DB,
+  PASS_DB: process.env.PASS_DB,
+  USER_DB: process.env.USER_DB,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+};
--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -4,7 +4,8 @@ const { config } = require('../config/secret');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb://${config.URL_DB}/${config.COLECTION_DB}`);
+  // await mongoose.connect(`mongodb://${config.URL_DB}/${config.COLECTION_DB}`);
+  await mongoose.connect(`mongodb+srv://${config.USER_DB}:${config.PASS_DB}@cluster0.berhsfm.mongodb.net/${config.COLECTION_DB}`);
   console.log("mongo connect shoes local");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
